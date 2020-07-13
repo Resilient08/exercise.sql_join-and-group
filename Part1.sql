26c3db5ebba1b3f2d8759432661fe88453f32eac
Resilient08 <58645288+Resilient08@users.noreply.github.com>
2020-07-13 14:13:01 -0400

    finished part 1

---
+++ Part1.sql
@@ -0,0 +1,4 @@
+SELECT customers.customerName, employees.lastName, employees.firstName
+FROM customers
+INNER JOIN employees ON customers.salesRepEmployeeNumber=employees.employeeNumber
+ORDER BY customerName ASC;
